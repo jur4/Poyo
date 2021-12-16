527671e7b0e4a895dab3476f950157f306484d4f
Julian <jramm88@web.de>
2021-12-16 22:16:47 +0100

    added so much stuff

---
+++ core/assets/Poyo/poyoTileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="poyoTileset" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="poyoTileset.png" width="128" height="32"/>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0869565" y="0.0434783" width="15.8696" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
